d3a8eabc7aaeef405d1cacf84cc2f80dc21211e8
Praça <134563140+pracataipas@users.noreply.github.com>
2025-11-08 16:47:58 +0000

    Update menuCtrl.js

--- src/app/control/menuCtrl.js
+++ src/app/control/menuCtrl.js
@@ -14,7 +14,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "AMENDOINS C/ MEL E SAL", price: "1"},
             {name: "TREMOÇOS", price: "0.5"}
        ]},
-        {name: "CAFETARIA", index: 2, isSelected: false, items: [
+        {name: "CAFETARIA", index: 1, isSelected: false, items: [
             {name: "CAFE", price: "1"},
             {name: "DESCAFEINADO", price: "1"},
             {name: "CARIOCA DE CAFE", price: "0.7"},
@@ -23,7 +23,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "CHA", price: "2"},
             {name: "CAPUCCINO", price: "2.5"}
         ]},
-        {name: "VINHOS", index: 1, isSelected: false, items: [
+        {name: "VINHOS", index: 2, isSelected: false, items: [
             {name: "- Maduro Branco", price: ""},
             {name: "GRAVIDADE", price: "23"},
             {name: "VAL MOREIRA", price: "23"},
@@ -52,7 +52,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "- Rosé", price: ""},
             {name: "BLEND.PT", price: "11"}
         ]},
-        {name: "SANGRIA", index: 2, isSelected: false, items: [
+        {name: "SANGRIA", index: 3, isSelected: false, items: [
             {name: "MARACUJÁ 1L", price: "15"},
             {name: "MARACUJÁ 2L", price: "25"},
             {name: "MAÇÃ E KIWI 1L", price: "15"},
@@ -62,7 +62,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "MANGA E MARACUJÁ 1L", price: "15"},
             {name: "MANGA E MARACUJÁ 2L", price: "25"}
         ]},
-        {name: "PONCHA", index: 3, isSelected: false, items: [
+        {name: "PONCHA", index: 4, isSelected: false, items: [
             {name: "MARACUJÁ", price: "4"},
             {name: "TANGERINA", price: "4"},
             {name: "REGIONAL", price: "4.5"},
@@ -70,7 +70,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "GARRAFA DE TANGERINA", price: "20"},
             {name: "GARRAFA DE REGIONAL", price: "22.5"}
         ]},
-        {name: "COCKTAIL", index: 4, isSelected: false, items: [
+        {name: "COCKTAIL", index: 5, isSelected: false, items: [
             {name: "MOJITO", price: "7"},
             {name: "CAIPIRINHA", price: "6"},
             {name: "OLD FASHIONED", price: "7"},
@@ -81,7 +81,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "maracujá, maçã, kiwi, coco, morango, framboesa e manga", price: ""},
             {name: "ESPRESSO MARTINI", price: "8"}
         ]},
-        {name: "CERVEJA", index: 5, isSelected: false, items: [
+        {name: "CERVEJA", index: 6, isSelected: false, items: [
             {name: "SUPER BOCK 20CL", price: "1.5"},
             {name: "SUPER BOCK 35CL", price: "2.5"},
             {name: "SUPER BOCK 50CL", price: "3"},
@@ -92,7 +92,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "SOMERSBY 33CL", price: "3"},
             {name: "frutos vermelhos, maçã", price: ""}
         ]},
-        {name: "GIN", index: 6, isSelected: false, items: [
+        {name: "GIN", index: 7, isSelected: false, items: [
             {name: "BEEFEATER", price: "6"},
             {name: "BOMBAY SAPHIRE", price: "6.5"},
             {name: "MARTIN MILLER'S", price: "7.5"},
@@ -102,7 +102,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "GIN MARE", price: "9"},
             {name: "MONKEY 47", price: "12"},
         ]},
-        {name: "WHISKY", index: 7, isSelected: false, items: [
+        {name: "WHISKY", index: 8, isSelected: false, items: [
             {name: "BALLANTINE'S", price: "5.5"},
             {name: "JAMESON", price: "6"},
             {name: "JACK DANIELS", price: "7"},
@@ -110,17 +110,17 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "MONKEY SHOULDER", price: "8"},
             {name: "OLD PARR 12 ANOS", price: "11"}
         ]},
-        {name: "VODKA", index: 8, isSelected: false, items: [
+        {name: "VODKA", index: 9, isSelected: false, items: [
             {name: "MOSKOVSKAYA", price: "5.5"},
             {name: "ABSOLUT", price: "7"},
             {name: "BELVEDERE", price: "8"},
             {name: "GREY GOOSE", price: "9"}
         ]},
-        {name: "RUM", index: 9, isSelected: false, items: [
+        {name: "RUM", index: 10, isSelected: false, items: [
             {name: "BACARDI LIMÓN", price: "5.5"},
             {name: "HAVANA CLUB 3 ANOS", price: "6"}
         ]},
-        {name: "APERITIVO E DIGESTIVO", index: 10, isSelected: false, items: [
+        {name: "APERITIVO E DIGESTIVO", index: 11, isSelected: false, items: [
             {name: "SHOT", price: "2.5"},
             {name: "SHOT PREMIUM", price: "3"},
             {name: "", price: ""},
@@ -131,12 +131,12 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "CROFT", price: "3.5"},
             {name: "CR&F", price: "4"}
         ]},
-        {name: "LICOR", index: 11, isSelected: false, items: [
+        {name: "LICOR", index: 12, isSelected: false, items: [
             {name: "AMÊNDOA AMARGA", price: "3.5"},
             {name: "LICOR BEIRÃO", price: "4"},
             {name: "BAILEYS", price: "4.5"}
         ]},
-        {name: "ÁGUA E SUMO", index: 12, isSelected: false, items: [
+        {name: "ÁGUA E SUMO", index: 13, isSelected: false, items: [
             {name: "ÁGUA", price: "1"},
             {name: "ÁGUA DAS PEDRAS", price: "1.2"},
             {name: "ÁGUA DAS PEDRAS SABORES", price: "1.5"},
@@ -149,7 +149,7 @@ menuApp.controller("menuCtrl", ['$scope', function ($scope) {
             {name: "SUMO DE LARANJA NATURAL", price: "3"},
             {name: "LIMONADA", price: "3"}
         ]},
-        {name: "SHISHA", index: 13, isSelected: false, items: [
+        {name: "SHISHA", index: 14, isSelected: false, items: [
             {name: "LOVE 66", price: "20"},
             {name: "melão, melancia, maracujá e menta", price: ""},
             {name: "L.KILL", price: "20"},
